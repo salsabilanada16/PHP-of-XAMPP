b1984e0fd33fbd3ef5b75a1dedac68114cb11e66
salsabilanada16 <sadzani16@gmail.com>
2020-02-07 14:46:19 +0700

    latihan 1 PHP of XAMPP dengan cara lain

---
+++ latihan1caralain.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pemrograman Web</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Hello World!</h1>";
+        echo "<p>Paragraf 1</p>"; 
+    ?>
+</body>
+</html>
