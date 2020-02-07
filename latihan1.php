7e2cb8b6e093fecd9560a8c02e3a42ed1550fb2a
salsabilanada16 <sadzani16@gmail.com>
2020-02-07 14:40:15 +0700

    latihan 1 PHP of xampp

---
+++ latihan1.php
@@ -0,0 +1,18 @@
+<?php
+// mencetak tulisan ke layar
+// bisa pake kutip 1 ataupun kutip 2
+    echo "<h1>Hello World!</h1>";
+    echo "<hr>";
+
+// kalau angka tidak usah pake kutip dan kalau menuliskan operasi matematika maka yang akan ditampilkan adalah hasil dari operasi tersebut
+// perintah echo adalah untuk mencetak ke layar kalau tidak ditulis maka tidak ditampilkan
+    echo (1 + 2) * (3 - 4);
+    echo '<hr>';
+
+// VARIABEL
+// --> tempat untuk menyimpan nilai
+// aturan variabel boleh ada angkanya tapi tidak boleh diawali dengan angka
+$
+
+
+?>
